9c1a0f4313a427718a8e3022e25cd1d2f82daff8
Firmansyah <taujago@gmail.com>
2017-10-19 16:00:58 +0800

    index.php kamfret..

--- index.php
+++ index.php
@@ -21,8 +21,8 @@
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
+	define('ENVIRONMENT', 'production');
 	// define('ENVIRONMENT', 'development');
-	define('ENVIRONMENT', 'development');
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
