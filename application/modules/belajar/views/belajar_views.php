b5a084b7b8eeea859e7a7b74279a5472f03e2035
NizarHafizulllah <nizarhafizullah66@gmail.com>
2017-10-20 19:28:48 +0800

    hapus modules tak terpakai

--- application/modules/belajar/views/belajar_views.php
+++
@@ -1,1 +0,0 @@
-yeyyy berhasil
--- application/modules/kec_ho/views/kec_ho_status_view_js.php
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript">
-
-		function printsurat(id){
-  			open('<?php echo site_url("$this->controller/printsuratizin?"); ?>'+'id='+ id);
-				}
-
-</script>
